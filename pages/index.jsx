af358f0506b359f1ca64173639b727e817274034
yeongjuyun <myyun1029@gmail.com>
2022-08-23 16:45:13 +0900

    [update] Main page 이미지 변경

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
 import { mainImg } from '../imgs';
 import Image from 'next/image';
-import { Grid } from '@mui/material';
 
-export default function HomePage(props) {
+export default function HomePage() {
   return (
-    <Grid item>
-      <Image src={mainImg} alt='main_image' width={1200} height={680} />
-    </Grid>
+    <>
+      <Image src={mainImg} alt='main_image' width={1300} height={680} />
+    </>
   );
 }
--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,27 +1,28 @@
 import { useState } from 'react';
 import StoreList from '../../components/stores/StoreList';
 import { useRouter } from 'next/router';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Container } from '@mui/material';
 
 export default function StoreListPage(props) {
   const router = useRouter();
   const [isModal, setIsModal] = useState(false);
 
   return (
-    <Grid item xs zeroMinWidth paddingX={15}>
-      <Typography
-        variant='h4'
-        component='h2'
-        paddingBottom={2}
-        marginBottom={8}
-        borderBottom='5px solid #2E3B55'
-        color='#253048'
-      >
-        <strong>Hunting List</strong>
-      </Typography>
-
-      <StoreList stores={props.stores} setIsModal={setIsModal} />
-    </Grid>
+    <Container maxWidth='xl'>
+      <Grid item zeroMinWidth>
+        <Typography
+          variant='h4'
+          component='h2'
+          paddingBottom={2}
+          marginBottom={8}
+          borderBottom='5px solid #2E3B55'
+          color='#253048'
+        >
+          <strong>Hunting List</strong>
+        </Typography>
+        <StoreList stores={props.stores} setIsModal={setIsModal} />
+      </Grid>
+    </Container>
   );
 }
 
