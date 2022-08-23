7c1dff68739ce920428db3d4d4a1080473d2d440
yeongjuyun <myyun1029@gmail.com>
2022-08-23 15:47:59 +0900

    [add] 이미지 추가

---
+++ imgs/index.js
@@ -0,0 +1,6 @@
+import mainImg from './main.png';
+import foodList from './foodList.png';
+import detailImg from './detail.png';
+import listImg from './List.png';
+
+export { mainImg, foodList, detailImg, listImg };
